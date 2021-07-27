aa7c798761cba8696120d4bdd41003754b567489
PitMac <jhondavidpitapita@hotmail.com>
2021-07-27 18:12:25 -0500

    todo api

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '2a48fa79ceb8662855002c802c53ca1a'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '4982471c-f3f1-4c4c-b93a-b186b3927452'
+};
